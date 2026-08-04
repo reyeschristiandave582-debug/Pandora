727896713bf1e33e7efccce2f1801be634affb08
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-04 13:43:45 +0800

    Update main-logo.tsx

--- src/components/sections/main-logo.tsx
+++ src/components/sections/main-logo.tsx
@@ -9,7 +9,7 @@ const MainLogo = () => {
       onClick={() => window.parent.postMessage({ type: "OPEN_EXTERNAL_URL", data: { url: "https://giftclick.org/aff_c?offer_id=174&aff_id=183864&source=Pandora" } }, "*")}
     >
       <img 
-        src="https://i.imgur.com/lfmzARL.png" 
+        src="https://i.imgur.com/k21q2NY.png" 
         alt="Pandora Logo" 
         className="h-10 sm:h-15 w-20 object-contain transition-all duration-700 hover:brightness-120"
       />
